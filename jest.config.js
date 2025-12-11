15b2f494fbc4bcd036a4b2fc711cd83281dd769a
huan <huanld@deha-soft.com>
2025-12-11 17:25:03 +0700

    fix CI-CD jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,16 @@
 module.exports = {
   preset: 'react-native',
 
-  // ============================================
-  // Test matching
-  // ============================================
-  testMatch: ['<rootDir>/__tests__/**/*.test. ts', '<rootDir>/__tests__/**/*.test.tsx'],
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts', '<rootDir>/__tests__/**/*.test.tsx'],
 
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 
-  // ============================================
-  // Setup
-  // ============================================
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
-  // ============================================
-  // Transform ignore patterns - FOR JEST 29
-  // ============================================
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native|@react-navigation|@react-native-community|@react-native-async-storage|react-redux|@reduxjs)/)',
   ],
 
-  // ============================================
-  // Module name mapper
-  // ============================================
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@api/(.*)$': '<rootDir>/src/api/$1',
@@ -31,39 +19,40 @@ module.exports = {
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
     '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
-    '^@redux/(. *)$': '<rootDir>/src/redux/$1',
+    '^@redux/(.*)$': '<rootDir>/src/redux/$1',
     '^@constants/(.*)$': '<rootDir>/src/constants/$1',
     '^@styles/(.*)$': '<rootDir>/src/styles/$1',
     '^@types/(.*)$': '<rootDir>/src/types/$1',
     '^@providers/(.*)$': '<rootDir>/src/providers/$1',
     '^@i18n/(.*)$': '<rootDir>/src/i18n/$1',
     '^@config/(.*)$': '<rootDir>/src/config/$1',
-    '^@locales/(. *)$': '<rootDir>/src/locales/$1',
-    '^@test-helpers/(. *)$': '<rootDir>/__tests__/helpers/$1',
+    '^@locales/(.*)$': '<rootDir>/src/locales/$1',
+    '^@test-helpers/(.*)$': '<rootDir>/__tests__/helpers/$1',
   },
 
-  // ============================================
-  // Module directories
-  // ============================================
   modulePaths: ['<rootDir>'],
   moduleDirectories: ['node_modules', 'src'],
 
-  // ============================================
-  // Coverage
-  // ============================================
   collectCoverage: false,
 
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.styles.{ts,tsx}',
     '!src/**/index.{ts,tsx}',
-    '!src/**/*. d.ts',
+    '!src/**/*.d.ts',
     '!src/**/__tests__/**',
   ],
 
   coverageDirectory: 'coverage',
 
-  coverageReporters: ['text', 'text-summary', 'lcov', 'html', 'cobertura', 'json-summary'],
+  coverageReporters: [
+    'text',
+    'text-summary',
+    'lcov',
+    'html',
+    'cobertura',
+    'json-summary',
+  ],
 
   coverageThreshold: {
     global: {
@@ -74,15 +63,12 @@ module.exports = {
     },
   },
 
-  // ============================================
-  // Reporters
-  // ============================================
   reporters: [
     'default',
     [
       'jest-junit',
       {
-        outputDirectory: '. /',
+        outputDirectory: './',
         outputName: 'junit.xml',
         classNameTemplate: '{classname}',
         titleTemplate: '{title}',
@@ -92,17 +78,11 @@ module.exports = {
     ],
   ],
 
-  // ============================================
-  // Other options
-  // ============================================
   verbose: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
   testTimeout: 10000,
 
-  // ============================================
-  // For Jest 29 - Fix for workers
-  // ============================================
   maxWorkers: '50%',
 };
